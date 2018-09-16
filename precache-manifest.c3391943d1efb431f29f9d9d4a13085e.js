9046baba10d6d886f6f33ed06dbf52d36c10f9db
Marc Lipscke <m.lipscke@markenstuermer.de>
2018-09-16 12:08:22 +0200

    deploy

---
+++ precache-manifest.c3391943d1efb431f29f9d9d4a13085e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-counter/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/vue-counter/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "396179743617230ee3ac",
+    "url": "/vue-counter/js/app.0ffaeaba.js"
+  },
+  {
+    "revision": "5d4fa1e9ffcdce99d6c05aeaaf239214",
+    "url": "/vue-counter/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-counter/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "396179743617230ee3ac",
+    "url": "/vue-counter/css/app.d649157f.css"
+  }
+];
